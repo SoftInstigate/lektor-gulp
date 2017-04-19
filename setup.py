9f76f6bc9e01cb41ff635b7300a17a2da8fdb38c
Maurizio Turatti <maurizio.turatti@gmail.com>
2017-04-19 04:34:32 +0200

    Add description in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',
     author=u'Maurizio Turatti',
     author_email='maurizio@softinstigate.com',
+    description=u'A simple Lektor plugin for gulp',
     url = "https://github.com/SoftInstigate/lektor-gulp",
     license='BSD',
     py_modules=['lektor_gulp'],
